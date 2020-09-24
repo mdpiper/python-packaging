9097c9c56984e00c59176d4bde2582f293e69416
Mark Piper <mark.piper@colorado.edu>
2020-09-24 13:42:42 -0600

    Back to development: 0.1.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = u'\n\n'.join(
 
 setup(
     name="amazeinator",
-    version="0.1.2",
+    version="0.1.3.dev0",
     description="An amazing Pyhton package",
     long_description=long_description,
     author="Mark Piper",
